dad1cc211c59b9bf2f161e41467fafb7859bc820
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-06-06 20:01:42 +0900

    jsファイルの不要なコメを削除

--- keihi/public/js/Incomerecord.js
+++ keihi/public/js/Incomerecord.js
@@ -21,24 +21,6 @@ new Vue({
             incomeInput[10] = document.getElementById('totalDeduction').value;
             incomeInput[11] = document.getElementById('netIncome').value;
 
-            // // 配列内に空文字があるかどうかの判定
-            // var result = incomeInput.some(item => item === null);
-            // // 下の条件式について実際に使ってチェック
-            // if (result === false) {
-            //     alert('未記入の項目があります。');
-            //     flg = false;
-            // }
-
-            // // 入力された金額が数値がどうかチェック
-            // var regex = new RegExp(/^-?[1-9][0-9]+$/);
-            // var numCheck = true;
-            // incomeInput.forEach(function (item, index, array) {
-            //     numCheck = regex.test(parseInt(incomeInput[index]));
-            //     if (numCheck === false) {
-            //         alert('0から始まらない数値でデータ入力を行ってください');
-            //     }
-            // })
-
             // 金額の整合性をチェック
             // 基本給と残業代、総支給金額との整合性をとる
             var calcTotalSalary = parseInt(incomeInput[3]) + parseInt(incomeInput[4]);
