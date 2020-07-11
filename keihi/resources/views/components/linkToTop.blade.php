5a6bd2ec4dcdd13f2784e5d280c6e0b2331f5cb8
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-07-11 21:29:56 +0900

    トップページ導線パーツに1つ前へリンクの追加

--- keihi/resources/views/components/linkToTop.blade.php
+++ keihi/resources/views/components/linkToTop.blade.php
@@ -1,3 +1,8 @@
-<div class="link">
-        <a href="<?php echo url('/'); ?>" id="link">アプリのトップページへ戻る</a>
+<div class="link" style="display:inline-flex">
+  <div style="margin-right:90px;">
+    <a href="<?php echo url('/'); ?>" id="link">アプリのトップページへ戻る</a>
+  </div>
+  <div>
+    <a href="<?php echo url()->previous(); ?>" id="link">1つ前のページへ戻る</a>
+  </div>
 </div>
