497a4413f287ff61e6f6f0aaf83f3bc1c2a50ea3
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-07-29 23:10:17 +0900

    賞与テーブルのマイグレーション実行

---
+++ keihi/database/migrations/2020_07_29_221733_create_bonuses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBonusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bonuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('totalBonus');
+            $table->integer('healthInsurance');
+            $table->integer('employeePension');
+            $table->integer('employmentInsurance');
+            $table->integer('incomeTax');
+            $table->integer('totalDeduction');
+            $table->integer('netIncome');
+            $table->integer('year');
+            $table->integer('month');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bonuses');
+    }
+}
